b371c38909f057c40145d81fffa5bae37494bd4b
王航威 <wanghangwei@youzan.com>
2018-12-10 12:38:35 +0800

    modify readme

--- GetQuestionAndroid.py
+++ GetQuestionAndroid.py
@@ -15,10 +15,10 @@ if __name__ == '__main__':
     while True:
         if current_step == 0:
             while True:
-                screenshot.tap_screen(952, 256)  # 进入全民品酒
+                screenshot.tap_screen(952, 256)  #进入全民品酒图标，右上角 自行修改位置
                 print("从全民品酒页切换一次，调整首页待品酒的位置")
                 time.sleep(0.5)
-                screenshot.tap_screen(64, 132)  # 返回到首页
+                screenshot.tap_screen(64, 132)  #返回到首页，左上角 自行修改位置
                 time.sleep(1)
                 screenshot.check_screenshot()
                 pos = find_ico_pos.find_ico_pos("./screenshot.png", "./resources/WinePoint_ico.png", current_step)
@@ -34,17 +34,17 @@ if __name__ == '__main__':
                     current_step = 1
                     break
         elif current_step == 1:
-            screenshot.tap_screen(952, 256)  # 进入全民品酒
+            screenshot.tap_screen(952, 256)  #进入全民品酒图标，右上角 自行修改位置
             time.sleep(1)
             qmpj_times = 0
             while True:
                 qmpj_times += 1
                 if qmpj_times % 10 == 3:
-                    screenshot.swipe_screen(516, 1780, 516, 760)
+                    screenshot.swipe_screen(516, 1780, 516, 760)   #往下滑动到底部，自行修改位置
                 if qmpj_times % 10 == 0:
-                    screenshot.swipe_screen(516, 360, 516, 1780)
+                    screenshot.swipe_screen(516, 360, 516, 1780)    #滑动到顶部，自行修改位置
                     time.sleep(1)
-                    screenshot.tap_screen(920, 1392)    # 点击刷新按钮
+                    screenshot.tap_screen(920, 1392)        #点击刷新按钮 自行修改位置
                     time.sleep(1)
                 if qmpj_times >= 50:
                     print('总次数达到50次，退出')
